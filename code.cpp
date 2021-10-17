49b9aad6a467e1ff8a36e3a650f30f9b8f9750a4
Ashish Meena <akm411201@gmail.com>
2021-10-17 21:06:51 +0530

    last leet

--- code.cpp
+++ code.cpp
@@ -19,7 +19,7 @@ public:
         }
         for(int i=0;i<nums.size()-1;i++)
         {
-            cout<<nums[i]<<" ";
+          //  cout<<nums[i]<<" ";
             if(nums[i]>=nums[i+1])return false;
         }
         return true;
