6df167e2a22875b80b2a3be45f387d9155f6c814
Ashish Meena <akm411201@gmail.com>
2021-10-17 21:08:42 +0530

    leet commit

--- code.cpp
+++ code.cpp
@@ -1,27 +1,72 @@
-class Solution {
+class Bank {
 public:
-    bool areNumbersAscending(string s)
+    vector<long long>bal;
+    long long n;
+    Bank(vector<long long>& balance)
     {
-        vector<int>nums;
-        for(int i=0;i<s.length();i++)
+        this->bal=balance;
+        this->n=balance.size();
+    }
+    
+    bool transfer(int account1, int account2, long long money) 
+    {
+         if(account1>=1&&account1<=n&&account2>=1&&account2<=n)
         {
-            string str="";
-            while(i<s.length())
+           if(bal[account1-1]>=money)
+           {
+               bal[account1-1]-=money;
+               bal[account2-1]+=money;
+               return true;
+           }
+            else
             {
-                if(s[i]==' ')break;
-                str+=s[i];
-                i++;
-            }
-            if(str[0]>='1'&&str[0]<='9')
-            {
-                nums.push_back(stoi(str));
+                return false;
             }
         }
-        for(int i=0;i<nums.size()-1;i++)
+        else
         {
-          //  cout<<nums[i]<<" ";
-            if(nums[i]>=nums[i+1])return false;
+            return false;
         }
-        return true;
+    }
+    
+    bool deposit(int account, long long money)
+    {
+        if(account>=1&&account<=n)
+        {
+            bal[account-1]+=money;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    bool withdraw(int account, long long money) 
+    {
+        if(account>=1&&account<=n)
+        {
+           if(bal[account-1]>=money)
+           {
+               bal[account-1]-=money;
+               return true;
+           }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 };
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
